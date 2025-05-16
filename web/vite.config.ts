d26464227a945be03acf03f53ec5e42a220e21cc
Rota911 <88843688+Rota911@users.noreply.github.com>
2025-05-16 15:43:29 +0200

    removed build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
 export default defineConfig({
   base: './',
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: path.join(__dirname, '..', 'web', 'build'),
-    assetsDir: 'assets',
-    emptyOutDir: true,
-  }
 })
